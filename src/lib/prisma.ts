d2c5655b9b9294d043a959d12151cd29ed25e57f
Rafal Kesik <rafal.kesik.g@gmail.com>
2026-08-13 11:36:28 +0200

    Add Zod validations

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,6 +3,11 @@ import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import { PrismaClient } from "../../generated/prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
+if (!connectionString) {
+  throw new Error(
+  "DATABASE_URL environment variable is not set"
+  )
+}
 
 const adapter = new PrismaBetterSqlite3({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
---
+++ src/validation/reservation.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const CreateReservationSchema = z.object({
+  name: z.string().min(1, "Name required").max(255),
+  platform: z.string().min(1, "Platform required").max(255),
+  guestAmount: z.int().positive("Must be at least 1 guest"),
+  startDate: z.date(),
+  endDate: z.date(),
+  note: z.string().max(255, "Note can have maximum 255 characters"),
+}).refine((data) => data.endDate > data.startDate, {
+  error: "End date must be after start date",
+  path: ["endDate"],
+});
+
+export type CreateReservation = z.infer<typeof CreateReservationSchema>;
