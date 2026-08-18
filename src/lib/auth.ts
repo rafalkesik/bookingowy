750f4bf8df1d152ae818b325c47fda704905bd81
Rafal Kesik <rafal.kesik.g@gmail.com>
2026-08-18 13:46:54 +0200

    Add roles to user model

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,12 +2,16 @@ import { betterAuth } from "better-auth/minimal";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
 import { nextCookies } from "better-auth/next-js";
+import { admin } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "sqlite",
   }),
-  plugins: [nextCookies()],
+  plugins: [
+    nextCookies(),
+    admin(),
+  ],
 
   emailAndPassword: {
     enabled: true,
