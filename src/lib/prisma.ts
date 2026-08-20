22021ed368145cce893005429eee2891c8b73076
Rafal Kesik <rafal.kesik.g@gmail.com>
2026-08-20 20:21:06 +0200

    Migrate to postgreSQL

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,16 @@
 import "dotenv/config";
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.POSTGRESQL_PRISMA_DATABASE_URL}`;
 if (!connectionString) {
   throw new Error(
-  "DATABASE_URL environment variable is not set"
+  "POSTGRESQL_DATABASE_URL environment variable is not set"
   )
 }
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
