bcb2cd9f7b6fb4bda1d20e9bc5af8f0419861734
Rafal Kesik <kesik.rafal@yahoo.com>
2026-08-22 18:36:42 +0200

    Translate calendar to Polsh

--- src/components/CalendarComponent.tsx
+++ src/components/CalendarComponent.tsx
@@ -2,10 +2,12 @@
 
 import { Calendar, View, Views, dayjsLocalizer } from "react-big-calendar";
 import dayjs from "dayjs";
+import polishLocale from "dayjs/locale/pl"
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useCallback, useState } from "react";
 import { RbcReservation } from "@/types/hostex";
 
+dayjs.locale(polishLocale)
 const localizer = dayjsLocalizer(dayjs);
 
 // Used to create custom views:
@@ -13,7 +15,7 @@ const localizer = dayjsLocalizer(dayjs);
 
 export const CalendarComponent = ({ events }: { events: RbcReservation[]}) => {
   const [date, setDate] = useState(new Date());
-  const [view, setView] = useState<View>(Views.MONTH)
+  const [view, setView] = useState<View>(Views.MONTH);
 
   const onNavigate = useCallback((newDate: Date) => setDate(newDate), []);
   const onView = useCallback((newView: View) => setView(newView), []);
@@ -28,6 +30,17 @@ export const CalendarComponent = ({ events }: { events: RbcReservation[]}) => {
         view={view}
         onNavigate={onNavigate}
         onView={onView}
+        culture="pl"
+        messages={{
+          month: "Miesiąc",
+          week: "Tydzień",
+          work_week: "Tydzień roboczy",
+          day: "Dzień",
+          agenda: "Agenda",
+          today: "Dziś",
+          previous: "Poprzedni",
+          next: "Następny",
+        }}
       />
     </div>
   )
