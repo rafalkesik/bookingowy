03027288298bc64321b9caf73c4e5488f84f655b
Rafal Kesik <kesik.rafal@yahoo.com>
2026-08-31 16:07:02 +0200

    Attempt to fix the deploy bug #3

--- src/components/react-big-calendar/CustomDateHeader.tsx
+++ src/components/react-big-calendar/CustomDateHeader.tsx
@@ -1,13 +1,23 @@
 // This file changes the default calendar component from 
 // external react-big-calendar component.
+// That's why I don't add specific type-validation for all props etc.
+// I assume that the component does it correctly.
 
 // This file adds the icons to the calendar.
 
 import dayjs from "dayjs";
+import PropTypes from 'prop-types';
 
 export default function CustomDateHeader(
   { label, drilldownView, onDrillDown, date, cleaningDays, cleaningAllowed } :
-  { cleaningDays: Set<string>, cleaningAllowed: boolean }
+  {
+    cleaningDays: Set<string>,
+    cleaningAllowed: boolean,
+    label: any,
+    date: any,
+    drilldownView: any,
+    onDrillDown: any,
+  }
 ) {
   const cleaningToday = cleaningDays.has(dayjs(date).format("YYYY-MM-DD"));
 
